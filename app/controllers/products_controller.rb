e0c453c24add38573ed68d748b88f64f5e413ea6
willamy388720 <willamygt@gmail.com>
2021-04-26 18:19:33 -0300

    product registration

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,24 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(params_product)
+    if @product.save
+      redirect_to products_path, success: 'Produto adicionado com sucesso'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_product
+    params.require(:product).permit(:description, :price, :image)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_26_184507) do
+ActiveRecord::Schema.define(version: 2021_04_26_201116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,7 +60,6 @@ ActiveRecord::Schema.define(version: 2021_04_26_184507) do
   create_table "products", force: :cascade do |t|
     t.string "description"
     t.decimal "price"
-    t.string "image_url"
     t.boolean "promotion"
     t.decimal "pecent_promotion"
     t.datetime "created_at", precision: 6, null: false
@@ -58,4 +78,5 @@ ActiveRecord::Schema.define(version: 2021_04_26_184507) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
