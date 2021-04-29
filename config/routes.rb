0041b4dec7ca525cb0f6ba12c36910cfcd162402
willamy388720 <willamygt@gmail.com>
2021-04-29 17:28:44 -0300

    renaming authentication routes and blocking some admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get '/sobre', to: 'about#index'
   resources :contacts, only: %i[new]
   resources :products
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, path: '/admin',
+                      path_names: { sign_in: 'entrar', sign_out: 'sair' }, skip: %i[registrations passwords]
+  devise_for :users, path: '/',
+                     path_names: { sign_in: 'entrar', sign_out: 'sair', sign_up: 'cadastrar' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
